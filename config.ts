dee662fc031794097824714bd97168c6d1e22080
Sanaz Bahmani <sanazbahmani35@gmail.com>
2023-12-06 14:06:24 +0330

    Create config file

---
+++ config.ts
@@ -0,0 +1,7 @@
+export const config = {
+    title: "Short H1 title of your <b>portfolio</b>",
+    subTitle:
+        "Subtitle, where you can further elaborate about the purpose of your work.",
+    navigation: ["Projects", "Contact"],
+    heroCTA: "Explore Projects",    
+};
